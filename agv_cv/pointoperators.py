3b61a4e30a3b4742775a554f46acedfb03fd638e
xiawaiting <xiawaiting@proton.me>
2023-09-17 21:51:48 +0000

    Refactor and cleanup

--- agv_cv/pointoperators.py
+++ agv_cv/pointoperators.py
@@ -7,4 +7,13 @@ def inversion(img):
 def addition(img, n):
     aimg = img + n
     np.clip(aimg, 0, 255, out=aimg)
-    
+    return aimg
+
+
+def scaling(img, k):
+    simg = k*img
+    np.clip(simg, 0, 255, out=simg)
+    return simg
+
+
+def
