2d47adaef90f050f39dc466706712b11e2ce41ac
xiawaiting <xiawaiting@proton.me>
2023-09-19 09:15:38 +0000

    add privacy policy

--- agv_cv/shadow_removal/test.cpp
+++ agv_cv/shadow_removal/test.cpp
@@ -4,4 +4,9 @@ int main(int argc, char* argv[]){
   
   cv::VideoCapture capture(argv[1]);
   if (!capture.isOpened()){
-      cv::VideoCapture c
+      cv::VideoCapture capture(argv[1]);
+  }
+  cv::Mat original;
+  cv::namedWindow("output",1);
+  while(1){
+    capture >> origi
