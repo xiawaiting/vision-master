a4e2a599e82fccbb6867d0d686d174cbff392034
xiawaiting <xiawaiting@proton.me>
2023-09-21 15:05:37 +0000

    v6.4.0

--- setup.py
+++ setup.py
@@ -3,4 +3,9 @@ from distutils.core import setup
 setup(name='agv_cv',
       version='0.1',
       packages=['agv_cv'],
-   
+      install_requires=[
+          'scipy',
+          'pillow',
+          'numpy',
+      ],
+      
